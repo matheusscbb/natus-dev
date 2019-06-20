d5b29517e2dc091f89ed2631930ac3a6f27c124a
RodrigoDisselli <disselli.rodrigo@gmail.com>
2019-06-20 00:32:28 -0300

    Alterações de Layout

--- js/go_to.js
+++ js/go_to.js
@@ -1,3 +1,9 @@
 function goTo(div) {
     document.getElementById(div).scrollIntoView({behavior: "smooth"})
+        if(div === "contact"){
+            setTimeout(function(){
+                document.getElementById("name").focus();
+            }, 750);
+             
+        }
 }
